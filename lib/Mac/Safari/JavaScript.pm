cde917605228954493d50dec0165574092d9f78a
Mark Fowler <mark@twoshortplanks.com>
2011-06-13 07:59:01 +0000

    Import of MARKF/Mac-Safari-JavaScript-1.00 from CPAN.
    gitpan-cpan-distribution: Mac-Safari-JavaScript
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         MARKF/Mac-Safari-JavaScript-1.00.tar.gz
    gitpan-cpan-author:       MARKF
    gitpan-cpan-maturity:     released

--- lib/Mac/Safari/JavaScript.pm
+++ lib/Mac/Safari/JavaScript.pm
@@ -10,9 +10,12 @@ use warnings;
 use Mac::AppleScript qw(RunAppleScript);
 use JSON::XS qw(decode_json);
 use Encode qw(encode decode);
+use Carp qw(croak);
+
+use Mac::Safari::JavaScript::Exception;
 
 our @EXPORT_OK;
-our $VERSION = "0.01";
+our $VERSION = "1.00";
 
 =head1 NAME
 
@@ -22,17 +25,20 @@ Mac::Safari::JavaScript - Run JavaScript in Safari on Mac OS X
 
   use Mac::Safarai::JavaScript qw(safari_js);
 
-  // do an alert
+  # do an alert
   safari_js 'alert("Hello Safari User")';
 
-  // return some value
+  # return some value
   var $arrayref = safari_js 'return [1,2,3]';
 
-  // multiple lines are okay
+  # multiple lines are okay
   safari_js <<'JAVASCRIPT';
     var fred = "bob";
     return fred;
   JAVASCRIPT
+  
+  # You can set variables to pass in
+  safari_js 'return document.getElementById(id).href', id => "mainlink";
 
 =head1 DESCRIPTION
 
@@ -42,38 +48,127 @@ browser on Mac OS X.
 The current implementation wraps the JavaScript in Applescript,
 compiles it, and executes it in order to control Safari.
 
-=head1 FUNCTION
+=head1 FUNCTIONS
+
+Functions are exported on request, or may be called fully qualified.
 
 =over
 
-=item safari_js($javascript)
+=item safari_js($javascript, @named_parameters)
 
 Runs the JavaScript in the first tab of the front window of the
 currently running Safari.
 
-Your code is automatically executed in a JavaScript function to
-avoid namespace polution.
+=over 8
 
-If you JavaScript returns something (via the C<return> keyword) then,
-as long as it can be represented by JSON, it will be returned as
-the result the function call.
+=item The script
+
+This script may safely contain newlines, unicode characters, comments etc.
+Any line numbers in error messages should match up with error messages
+
+=item Return value
+
+C<safari_js> will do a passable job of mapping whatever you returned from
+your JavaScript (using the C<return> keyword) into a Perl data structure it
+will return.  If you do not return a value from JavaScript (i.e. the return
+keyword is not executed) then C<safari_js> will return the empty list.  If
+you return nothing (i.e. use C<return;> in your script), C<safari_js> will
+return C<undef>.
+
+Whatever you return from your JavaScript will be encoded into JSON with
+Safari's native C<JSON.stringify> function and decoded on the Perl side
+using the JSON::XS module.
+
+JavaScript data structures are mapped as you might expect:  Objects to
+hashrefs, Arrays to arrayrefs, strings and numbers to their normal scalar
+representation, and C<null>, C<true> and C<false> to C<undef>, JSON::XS::true
+(which you can treat like the scalar C<1>) and JSON::XS::false (which you
+can treat like the scalar C<0>) respectivly.  Please see L<JSON::XS>
+for more information.
+
+You cannot return anything from JavaScript that has a ciruclar reference
+in it (as this cannot be represented by JSON.)
+
+=item Passing Parameters
+
+You may pass in named parameters by passing them as name/value pairs
+
+  safari_js $js_code_to_run, name1 => $value1, name2 => $value2, ...
+
+The parameters are simply availble as variables in your code.
+
+Internally parameters are converted from Perl data structures into JavaScript
+using JSON::XS using the reverse mapping described above.  You may not pass
+in circular data structures.  Again, see L<JSON::XS> for more infomation.
+
+=item Exception Handling
+
+If what you pass causes an uncaught exception within the Safari web browser
+(including exceptions during by parsing your script) then a
+Mac::Safari::JavaScript::Exception exception object will be raised by
+C<safari_js>.  This will stringify to the exception you normally would see
+in your browser and can be integated for extra info such as the line number,
+etc.
+
+=back
 
 =cut
 
-sub safari_js($) {
+sub safari_js($;@) {
   my $javascript = shift;
 
-  # wrap the javascript in helper functions so we always
-  # return a javascript string in order to be consistent
+  # create a coder objects
+  my $coder = JSON::XS->new;
+  $coder->allow_nonref(1);
+
+  # handle the arguments passed in
+  if (@_ % 2) {
+    croak "Uneven number of parameters passed to safari_js";
+  }
+  my %params;
+  while (@_) {
+    my $key = shift;
+    if (exists $params{ $key }) {
+      croak "Duplicate parameter '$key' passed twice to safari_js";
+    }
+    # we're going to put the value into a string to
+    # eval.  This means we need to escape all the meta chars
+    my $value = $coder->encode(shift);
+    $value =~ s/\\/\\\\/gx;  # \ -> \\
+    $value =~ s/"/\\"/gx;    # " -> \"
+
+    $params{ $key } = $value;
+  }
+  my $args = join ",", keys %params;
+  my $values = join ",", values %params;
+
+  # we're going to put the javascript into a string to
+  # eval.  This means we need to escape all the meta chars
+  $javascript =~ s/\\/\\\\/gx;  # \ -> \\
+  $javascript =~ s/"/\\"/gx;    # " -> \"
+
+  # since we've now effectivly got a multiline string (and
+  # JavaScript doesn't support that) we better fix that up
+  # Note that we're trying not to mess up the line numers
+  # inside the eval of what we were passed
+  $javascript = join '\\n"+"',split /\n/x, $javascript;
+
+  # wrap the javascript in helper functions
+  #  
+  #  - use (function () { })() to avoid poluting the global namespace
+  #  - use eval "" to allow syntax errors to be caught and returned as a
+  #    data structure we can re-throw on the Perl side
+  #  - JSON.stringify(undefined) returns, not the string "null".  We detect
+  #    this and return the string "null"
+
   $javascript = <<"ENDOFJAVASCRIPT";
-  JSON.stringify((function () { $javascript; })());
+try{var result=eval("JSON.stringify((function($args){ $javascript;throw'NothingReturned'})($values));");(result===undefined)?'{"undefined":1}':'{"result":'+result+'}';}catch (e){ (e == "NothingReturned")?'{"noresult":1}':'{"error":'+JSON.stringify(e)+'}'; }
 ENDOFJAVASCRIPT
 
-  # escape the backslashes
-  $javascript =~ s/\\/\\\\/xg;
-
-  # escape the quotes
-  $javascript =~ s/"/\\"/xg;
+  # escape the string escapes again as we're going to pass
+  # the whole thing via Applescript now
+  $javascript =~ s/\\/\\\\/gx;     # \ -> \\
+  $javascript =~ s/"/\\"/gx;       # " -> \"
 
   # wrap it in applescript
   my $applescript = <<"ENDOFAPPLESCRIPT";
@@ -104,9 +199,13 @@ ENDOFAPPLESCRIPT
   $json =~ s/\\\\/\\/gx;
 
   # and decode this from json
-  my $coder = JSON::XS->new;
-  $coder->allow_nonref(1);
-  return $coder->decode($json);
+  my $ds = $coder->decode($json);
+
+  return undef if exists $ds->{undefined};
+  return if exists $ds->{noresult};
+  return $ds->{result} if exists $ds->{result};
+  croak(Mac::Safari::JavaScript::Exception->new(%{ $ds->{error} })) if exists $ds->{error};
+  croak("Unexpected error");
 }
 push @EXPORT_OK, "safari_js";
 
@@ -127,7 +226,7 @@ Bugs should be reported to me via the CPAN RT system. http://rt.cpan.org/NoAuth/
 
 =head1 SEE ALSO
 
-L<Mac::Applescript>
+L<Mac::Applescript>, L<Mac::Safari::JavaScript::Exception>
 
 =cut
 
--- t/basic.t
+++ t/basic.t
@@ -3,21 +3,104 @@
 use strict;
 use warnings;
 
-use Test::More tests => 10;
+use Test::More tests => 20;
 use Mac::Safari::JavaScript qw(safari_js);
+use Scalar::Util qw(blessed);
+use Data::Dumper;
 
-is_deeply safari_js "return 'string'", "string", "string";
-is_deeply safari_js "return 1+2+3" , 6, "number";
-ok safari_js "return true", "truth, true";
-ok !safari_js "return false", "truth, false";
-is_deeply safari_js "return { 'a': 'b'}" , { a => "b"}, "object/hash";
-is_deeply safari_js "return [1,2,3]" , [1,2,3], "object/array";
-is_deeply safari_js 'return "L\\u00e9on"', "L\x{e9}on", "unicode \\u sequence";
-is safari_js "return 'L\x{e9}on'.length", 4, "length";
-is safari_js "return '\x{2603}'", "\x{2603}", "snowman roundtrip";
-is safari_js <<'JAVASCRIPT', "bobbuzz", "multiline";
+########################################################################
+# return values
+########################################################################
+
+# simple values returned
+is_deeply safari_js("return 'string'"), "string", "string";
+is_deeply safari_js('return "string"'), "string", "string";
+is_deeply safari_js("return 1+2+3"), 6, "number";
+
+# truth values returned
+ok safari_js("return true"), "truth, true";
+ok !safari_js("return false"), "truth, false";
+
+# return simple object
+is_deeply safari_js("return { 'a': 'b'}") , { a => "b"}, "object/hash";
+is_deeply safari_js("return [1,2,3]") , [1,2,3], "object/array";
+
+# return various forms of undef
+is_deeply [safari_js "1+1"], [], "doesn't return anything";
+is_deeply [safari_js "return"], [undef], "bare return";
+is_deeply [safari_js "return undefined"], [undef], "return undefined";
+is_deeply [safari_js "return null"], [undef], "return null";
+
+# unicode
+is_deeply safari_js('return "L\\u00e9on"'), "L\x{e9}on", "unicode \\u sequence";
+is safari_js("return 'L\x{e9}on'.length"), 4, "length";
+is safari_js("return '\x{2603}'"), "\x{2603}", "snowman roundtrip";
+
+# multiline, including a comment
+is safari_js(<<'JAVASCRIPT'), "bobbuzz", "multiline";
 // this is a multi line example
 var foo = "bob";
 var bar = "buzz";
 return foo+bar;
 JAVASCRIPT
+
+########################################################################
+# passing things in
+########################################################################
+
+is_deeply [safari_js "return foo + bar", foo => 2, bar => 3], [5], "basic in";
+is_deeply [safari_js "return [number,hash,array, truth,falsehood,nully]",
+
+  number => 6,
+
+  hash => { foo => 1, bar => "a", baz => JSON::XS::true, buzz => JSON::XS::false, bizz => undef, array => [1,"a", JSON::XS::true, buzz => JSON::XS::false, undef, "\x{e9}" ], hash => { foo => 1, bar => "a", baz => JSON::XS::true, buzz => JSON::XS::false, bizz => undef, array => [1,"a", JSON::XS::true, buzz => JSON::XS::false, undef, "\x{e9}" ] } },
+
+  array => [1,"a", JSON::XS::true, buzz => JSON::XS::false, undef, "\x{e9}", { foo => 1, bar => "a", baz => JSON::XS::true, buzz => JSON::XS::false, bizz => undef, array => [1,"a", JSON::XS::true, buzz => JSON::XS::false, undef, "\x{e9}" ], hash => { foo => 1, bar => "a", baz => JSON::XS::true, buzz => JSON::XS::false, bizz => undef, array => [1,"a", JSON::XS::true, buzz => JSON::XS::false, undef, "\x{e9}" ]}}],
+
+  truth => JSON::XS::true,
+
+  falsehood => JSON::XS::false,
+
+  nully => undef,
+], [[
+  6,
+  
+  { foo => 1, bar => "a", baz => JSON::XS::true, buzz => JSON::XS::false, bizz => undef, array => [1,"a", JSON::XS::true, buzz => JSON::XS::false, undef, "\x{e9}" ], hash => { foo => 1, bar => "a", baz => JSON::XS::true, buzz => JSON::XS::false, bizz => undef, array => [1,"a", JSON::XS::true, buzz => JSON::XS::false, undef, "\x{e9}" ] } },
+
+  [1,"a", JSON::XS::true, buzz => JSON::XS::false, undef, "\x{e9}", { foo => 1, bar => "a", baz => JSON::XS::true, buzz => JSON::XS::false, bizz => undef, array => [1,"a", JSON::XS::true, buzz => JSON::XS::false, undef, "\x{e9}" ], hash => { foo => 1, bar => "a", baz => JSON::XS::true, buzz => JSON::XS::false, bizz => undef, array => [1,"a", JSON::XS::true, buzz => JSON::XS::false, undef, "\x{e9}" ]}}],
+
+  JSON::XS::true,
+
+  JSON::XS::false,
+
+  undef,
+]], "round trip";
+
+eval { 
+  safari_js "return true;", "uneven"
+};
+like ($@, qr/Uneven number of parameters passed to safari_js/, "Uneven number of parameters");
+
+########################################################################
+
+# testing exceptions
+sub error_like (&$$) {
+  local $Test::Builder::Level = $Test::Builder::Level + 1;
+  my $uboat = shift;
+  my $re = shift;
+  my $description = shift;
+  eval { $uboat->() };
+  unless (defined($@) && blessed($@) && $@->isa("Mac::JavaScript::Safari::Exception") && "$@" =~ $re) {
+    ok(1, $description);
+    return 0;
+  }
+  return ok(1, $description);
+}
+
+error_like {
+  safari_js "++++";
+} qr/Parse error/, "invalid js";
+
+error_like {
+  safari_js "{";
+} qr/Parse error/, "stray }";
